4b3894393027a86df91248388390e428c8d46ddd
simon <124110714+Teckwithsi@users.noreply.github.com>
2023-01-31 20:53:01 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+print ("welcome to a small guesing game ")
+print("it is a small game and just a little fun ")
+
+player = input("what is the capital of the uk ")
+
+
+if player == "london":
+    print("yes")
+else:
+    print("no")
+
+
+player = input("what is the first day of the week? ")
+
+if player =="monday":
+    print("yes")
+else:
+    print("no")
+
+player = input("what is the last day of the week? ")
+
+
+if player =="sunday":
+    print("yes")
+else:
+    print("no")
+
+
+player = input ("what is the last latter of coffee? ")
+
+if player == "e":
+    print("yes")
+else:
+    print("no")
+
+
+player = input ("what language is this coded in? ")
+
+
+if player == "python":
+    print("yes")
+else:
+    print ("no")
+
+player = input("what is the capital of the usa")
+
+if player == "washington":
+    print ("yes")
+else:
+    print("no ")
